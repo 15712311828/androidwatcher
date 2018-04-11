efbcfbc5345cde2277c7a992287b58549d8db664
haoyan.zhang <haoyan.zhang@qunar.com>
2018-04-11 16:57:58 +0800

    7

--- src/main/java/com/androidwatcher/common/RtmpHandler.java
+++ src/main/java/com/androidwatcher/common/RtmpHandler.java
@@ -2,6 +2,11 @@ package com.androidwatcher.common;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.avcodec;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_objdetect;
+import org.bytedeco.javacv.*;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,6 +23,8 @@ public class RtmpHandler {
     private static ExecutorService fixedThreadPool = Executors.newFixedThreadPool(8);
 
     static {
+        Loader.load(opencv_objdetect.class);
+
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
@@ -48,11 +55,61 @@ public class RtmpHandler {
 
         log.info("thread name {}",Thread.currentThread().getName());
         threads.put(name,Thread.currentThread());
+        FrameGrabber grabber=null;
+        FrameRecorder recorder=null;
+        try {
+            long startTime = 0;
+            grabber = FFmpegFrameGrabber.createDefault("rtmp://118.89.229.227/live/"+name);
+            grabber.start();
+            OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+            //如果想要保存图片,可以使用 opencv_imgcodecs.cvSaveImage("hello.jpg", grabbedImage);来保存图片
+            recorder =  FrameRecorder.createDefault("rtmp://118.89.229.227/live/"+name+"handled", 640, 360);
+            recorder.setVideoCodec(avcodec.AV_CODEC_ID_H264);
+            recorder.setFormat("flv");
+            recorder.setFrameRate(15);
+            recorder.setGopSize(15);
+            log.info("准备开始推流...");
+            recorder.start();
+            log.info("开始推流");
+            Frame grabframe;
+            opencv_core.IplImage grabbedImage;
+            while (!Thread.currentThread().isInterrupted()&&(grabframe = grabber.grab()) != null) {
+                grabbedImage = converter.convert(grabframe);
+                Frame rotatedFrame = converter.convert(grabbedImage);
 
-        while(!Thread.currentThread().isInterrupted()){
-            log.info("{} run",name);
+                if (startTime == 0) {
+                    startTime = System.currentTimeMillis();
+                }
+                recorder.setTimestamp(1000 * (System.currentTimeMillis() - startTime));
+                if (rotatedFrame != null) {
+                    recorder.record(rotatedFrame);
+                }
 
+                Thread.sleep(10);
+            }
         }
-        log.info("{} exit",name);
+        catch (Exception e) {
+            log.error("rtmp fail", e);
+        }
+        finally {
+            try {
+                if (recorder != null) {
+                    recorder.stop();
+                    recorder.release();
+                }
+            }
+            catch (Exception e) {
+                log.error("recorder close fail", e);
+            }
+            try {
+                if (grabber != null) {
+                    grabber.stop();
+                }
+            }
+            catch (Exception e) {
+                log.error("grabber close fail", e);
+            }
+        }
+        log.info("{} rtmp exit",name);
     }
 }
